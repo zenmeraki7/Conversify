844e1bd40e069103892393172f226e954dae43d1
zenmeraki7 <zenmerakihelp@gmail.com>
2025-07-02 13:15:54 +0530

    conversify

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Conversify</h1>
+        <p>Welcome to Conversify, your AI-powered conversation assistant!</p>
+      </div>     
+  </>
+  )
+}
+
+export default App
